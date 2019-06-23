b63221388eeedb6e94675fe4c0a6e4a620b6f515
Alberto <info@androiddesdecero.com>
2019-06-23 17:50:11 +0200

    09

---
+++ app/src/main/java/com/androiddesdecero/arquitecturamvvm/model/RepoSearchResul.java
@@ -0,0 +1,24 @@
+package com.androiddesdecero.arquitecturamvvm.model;
+
+import androidx.room.Entity;
+import androidx.room.TypeConverters;
+
+import com.androiddesdecero.arquitecturamvvm.db.GithubTypeConverters;
+
+import java.util.List;
+
+@Entity(primaryKeys = {"query"})
+@TypeConverters(GithubTypeConverters.class)
+public class RepoSearchResul {
+    public final String query;
+    public final List<Integer> repoIds;
+    public final int totalCount;
+    public final Integer next;
+
+    public RepoSearchResul(String query, List<Integer> repoIds, int totalCount, Integer next) {
+        this.query = query;
+        this.repoIds = repoIds;
+        this.totalCount = totalCount;
+        this.next = next;
+    }
+}
