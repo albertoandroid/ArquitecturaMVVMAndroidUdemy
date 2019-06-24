44ca110237c3a1fef38241deb88390420d3f289c
Alberto <info@androiddesdecero.com>
2019-06-24 22:03:26 +0200

    013

--- app/src/main/java/com/androiddesdecero/arquitecturamvvm/db/RepoDao.java
+++ app/src/main/java/com/androiddesdecero/arquitecturamvvm/db/RepoDao.java
@@ -1,6 +1,10 @@
 package com.androiddesdecero.arquitecturamvvm.db;
 
+import android.util.SparseIntArray;
+
+import androidx.arch.core.util.Function;
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Transformations;
 import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
@@ -10,6 +14,8 @@ import com.androiddesdecero.arquitecturamvvm.model.Contributor;
 import com.androiddesdecero.arquitecturamvvm.model.Repo;
 import com.androiddesdecero.arquitecturamvvm.model.RepoSearchResult;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 @Dao
@@ -35,6 +41,28 @@ public abstract class RepoDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     public abstract void insert(RepoSearchResult result);
+
+    public LiveData<List<Repo>> loadOrdered(List<Integer> repoIds){
+        SparseIntArray order = new SparseIntArray();
+        int index = 0;
+        for(Integer repoId: repoIds){
+            order.put(repoId, index++);
+        }
+        return Transformations.map(loadById(repoIds), new Function<List<Repo>, List<Repo>>() {
+            @Override
+            public List<Repo> apply(List<Repo> repositories) {
+                Collections.sort(repositories, new Comparator<Repo>() {
+                    @Override
+                    public int compare(Repo o1, Repo o2) {
+                        int pos1 = order.get(o1.id);
+                        int pos2 = order.get(o2.id);
+                        return pos1 - pos2;
+                    }
+                });
+                return repositories;
+            }
+        });
+    }
 
     @Query("SELECT * FROM RepoSearchResult WHERE query = :query")
     public abstract LiveData<RepoSearchResult> search(String query);
