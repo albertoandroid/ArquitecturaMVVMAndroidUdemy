14a992f704765e5e0b0ee524c863e19d94b58b14
Alberto <info@androiddesdecero.com>
2019-06-25 19:48:28 +0200

    18

--- app/src/main/java/com/androiddesdecero/arquitecturamvvm/api/WebServiceApi.java
+++ app/src/main/java/com/androiddesdecero/arquitecturamvvm/api/WebServiceApi.java
@@ -2,13 +2,17 @@ package com.androiddesdecero.arquitecturamvvm.api;
 
 import androidx.lifecycle.LiveData;
 
+import com.androiddesdecero.arquitecturamvvm.model.Contributor;
 import com.androiddesdecero.arquitecturamvvm.model.Repo;
+import com.androiddesdecero.arquitecturamvvm.model.RepoSearchResponse;
 import com.androiddesdecero.arquitecturamvvm.model.User;
 
 import java.util.List;
 
+import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface WebServiceApi {
 
@@ -20,4 +24,13 @@ public interface WebServiceApi {
 
     @GET("repos/{owner}/{name}")
     LiveData<ApiResponse<Repo>> getRepo(@Path("owner") String owner, @Path("name") String name);
+
+    @GET("repos/{owner}/{name}/contributors")
+    LiveData<ApiResponse<List<Contributor>>> getContributors(@Path("owner") String owner, @Path("name") String name);
+
+    @GET("search/repositories")
+    LiveData<ApiResponse<RepoSearchResponse>> searchRepos(@Query("q") String query);
+
+    @GET("search/repositories")
+    Call<RepoSearchResponse> searchRepos(@Query("q") String query, @Query("page") int page);
 }
