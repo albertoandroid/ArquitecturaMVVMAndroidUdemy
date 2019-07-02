8ae343087cfbb47fce6c77e728c11002979bcac8
Alberto <info@androiddesdecero.com>
2019-07-02 18:06:43 +0200

    50

--- app/src/main/java/com/androiddesdecero/arquitecturamvvm/ui/common/DataBoundListAdapter.java
+++ app/src/main/java/com/androiddesdecero/arquitecturamvvm/ui/common/DataBoundListAdapter.java
@@ -1,8 +1,11 @@
 package com.androiddesdecero.arquitecturamvvm.ui.common;
 
+import android.os.AsyncTask;
 import android.view.ViewGroup;
 
+import androidx.annotation.MainThread;
 import androidx.databinding.ViewDataBinding;
+import androidx.recyclerview.widget.DiffUtil;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
@@ -27,4 +30,71 @@ public abstract class DataBoundListAdapter<T, V extends ViewDataBinding> extends
     }
 
     protected abstract void bind(V binding, T item);
+
+    @MainThread
+    public void replace(List<T> update){
+        dataVersion++;
+        if(items == null){
+            if(update == null){
+                return;
+            }
+            items = update;
+            notifyDataSetChanged();
+        } else if(update == null){
+            int oldSize = items.size();
+            items = null;
+            notifyItemRangeRemoved(0, oldSize);
+        } else {
+            final int startVersion = dataVersion;
+            final List<T> oldItems = items;
+            new AsyncTask<Void, Void, DiffUtil.DiffResult>(){
+
+                @Override
+                protected DiffUtil.DiffResult doInBackground(Void... voids) {
+                    return DiffUtil.calculateDiff(new DiffUtil.Callback() {
+                        @Override
+                        public int getOldListSize() {
+                            return oldItems.size();
+                        }
+
+                        @Override
+                        public int getNewListSize() {
+                            return update.size();
+                        }
+
+                        @Override
+                        public boolean areItemsTheSame(int oldItemPosition, int newItemPosition) {
+                            T oldItem = oldItems.get(oldItemPosition);
+                            T newItem = update.get(newItemPosition);
+                            return DataBoundListAdapter.this.areItemsTheSame(oldItem, newItem);
+                        }
+
+                        @Override
+                        public boolean areContentsTheSame(int oldItemPosition, int newItemPosition) {
+                            T oldItem = oldItems.get(oldItemPosition);
+                            T newItem = update.get(newItemPosition);
+                            return DataBoundListAdapter.this.areContentsTheSame(oldItem, newItem);
+                        }
+                    });
+                }
+
+                @Override
+                protected void onPostExecute(DiffUtil.DiffResult diffResult){
+                    if(startVersion != dataVersion){
+                        return;
+                    }
+                    items = update;
+                    diffResult.dispatchUpdatesTo(DataBoundListAdapter.this);
+                }
+            }.execute();
+        }
+    }
+
+    protected abstract boolean areItemsTheSame(T oldItem, T newItem);
+    protected abstract boolean areContentsTheSame(T oldItem, T newItem);
+
+    @Override
+    public int getItemCount(){
+        return items == null ? 0 : items.size();
+    }
 }
