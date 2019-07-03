595e7d887fb04dbb65f6498db40f51cba4667330
Alberto <info@androiddesdecero.com>
2019-07-03 19:16:27 +0200

    69

---
+++ app/src/main/java/com/androiddesdecero/arquitecturamvvm/GithubApp.java
@@ -0,0 +1,28 @@
+package com.androiddesdecero.arquitecturamvvm;
+
+import android.app.Activity;
+import android.app.Application;
+
+import javax.inject.Inject;
+
+import dagger.android.AndroidInjector;
+import dagger.android.DispatchingAndroidInjector;
+import dagger.android.HasActivityInjector;
+
+public class GithubApp extends Application implements HasActivityInjector {
+
+    @Inject
+    DispatchingAndroidInjector<Activity> dispatchingAndroidInjector;
+
+    @Override
+    public void onCreate(){
+        super.onCreate();
+        AppInjector.init(this);
+    }
+
+
+    @Override
+    public DispatchingAndroidInjector<Activity> activityInjector() {
+        return dispatchingAndroidInjector;
+    }
+}
