ee6dec9088a158667172086c0607e198ace13433
Alberto <info@androiddesdecero.com>
2019-07-03 17:51:14 +0200

    65

--- app/src/main/java/com/androiddesdecero/arquitecturamvvm/viewmodel/GithubViewModelFactory.java
+++ app/src/main/java/com/androiddesdecero/arquitecturamvvm/viewmodel/GithubViewModelFactory.java
@@ -1,5 +1,6 @@
 package com.androiddesdecero.arquitecturamvvm.viewmodel;
 
+import androidx.annotation.NonNull;
 import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -17,5 +18,27 @@ public class GithubViewModelFactory implements ViewModelProvider.Factory {
     @Inject
     public GithubViewModelFactory(Map<Class<? extends ViewModel>, Provider<ViewModel>> creators){
         this.creators = creators;
+    }
+
+    @NonNull
+    @Override
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+        Provider<? extends ViewModel> creator = creators.get(modelClass);
+        if(creator == null){
+            for(Map.Entry<Class<? extends ViewModel>, Provider<ViewModel>> entry : creators.entrySet()){
+                if(modelClass.isAssignableFrom(entry.getKey())){
+                    creator = entry.getValue();
+                    break;
+                }
+            }
+        }
+        if(creator == null){
+            throw new IllegalArgumentException("unknow model class " + modelClass);
+        }
+        try {
+            return (T) creator.get();
+        }catch (Exception e){
+            throw new RuntimeException(e);
+        }
     }
 }
