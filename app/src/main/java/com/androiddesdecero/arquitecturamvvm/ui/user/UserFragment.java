59fce18effdf4cbb138262d3a147fa7eaca41021
Alberto <info@androiddesdecero.com>
2019-07-06 10:28:27 +0200

    81

--- app/src/main/java/com/androiddesdecero/arquitecturamvvm/ui/user/UserFragment.java
+++ app/src/main/java/com/androiddesdecero/arquitecturamvvm/ui/user/UserFragment.java
@@ -3,10 +3,13 @@ package com.androiddesdecero.arquitecturamvvm.ui.user;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingComponent;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,10 +19,15 @@ import com.androiddesdecero.arquitecturamvvm.R;
 import com.androiddesdecero.arquitecturamvvm.binding.FragmentDataBindingComponent;
 import com.androiddesdecero.arquitecturamvvm.databinding.FragmentUserBinding;
 import com.androiddesdecero.arquitecturamvvm.di.Injectable;
+import com.androiddesdecero.arquitecturamvvm.model.Repo;
+import com.androiddesdecero.arquitecturamvvm.model.User;
+import com.androiddesdecero.arquitecturamvvm.repository.Resource;
 import com.androiddesdecero.arquitecturamvvm.ui.common.NavigationController;
 import com.androiddesdecero.arquitecturamvvm.ui.common.RepoListAdapter;
 import com.androiddesdecero.arquitecturamvvm.ui.common.RetryCall;
 import com.androiddesdecero.arquitecturamvvm.utils.AutoClearedValue;
+
+import java.util.List;
 
 import javax.inject.Inject;
 
@@ -70,4 +78,41 @@ public class UserFragment extends Fragment implements Injectable {
         return dataBinding.getRoot();
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        userViewModel = ViewModelProviders.of(this, viewModelFactory).get(UserViewModel.class);
+        userViewModel.setLogin(getArguments().getString(LOGIN_KEY));
+        userViewModel.getUser().observe(this, new Observer<Resource<User>>() {
+            @Override
+            public void onChanged(Resource<User> userResource) {
+                binding.get().setUser(userResource == null ? null : userResource.data);
+                binding.get().setUserResource(userResource);
+                binding.get().executePendingBindings();
+            }
+        });
+        RepoListAdapter rvAdapter = new RepoListAdapter(dataBindingComponent, false, new RepoListAdapter.RepoClickCallback() {
+            @Override
+            public void onClick(Repo repo) {
+                navigationController.navigateToRepo(repo.owner.login, repo.name);
+            }
+        });
+        binding.get().repoList.setAdapter(rvAdapter);
+        this.adapter = new AutoClearedValue<>(this, rvAdapter);
+        initRepoList();
+    }
+
+    private void initRepoList(){
+        userViewModel.getRepositories().observe(this, new Observer<Resource<List<Repo>>>() {
+            @Override
+            public void onChanged(Resource<List<Repo>> repos) {
+                if(repos == null){
+                    adapter.get().replace(null);
+                } else {
+                    adapter.get().replace(repos.data);
+                }
+            }
+        });
+    }
 }
