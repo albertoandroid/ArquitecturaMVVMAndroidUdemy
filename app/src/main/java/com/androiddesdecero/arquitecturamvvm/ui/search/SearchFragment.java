3edd744ef35efee386395ff505ec8901505254d8
Alberto <info@androiddesdecero.com>
2019-07-06 09:05:22 +0200

    74

--- app/src/main/java/com/androiddesdecero/arquitecturamvvm/ui/search/SearchFragment.java
+++ app/src/main/java/com/androiddesdecero/arquitecturamvvm/ui/search/SearchFragment.java
@@ -3,21 +3,28 @@ package com.androiddesdecero.arquitecturamvvm.ui.search;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingComponent;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
 
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.TextView;
 
 import com.androiddesdecero.arquitecturamvvm.R;
 import com.androiddesdecero.arquitecturamvvm.binding.FragmentDataBindingComponent;
 import com.androiddesdecero.arquitecturamvvm.databinding.FragmentSearchBinding;
 import com.androiddesdecero.arquitecturamvvm.di.Injectable;
+import com.androiddesdecero.arquitecturamvvm.model.Repo;
 import com.androiddesdecero.arquitecturamvvm.ui.common.NavigationController;
 import com.androiddesdecero.arquitecturamvvm.ui.common.RepoListAdapter;
+import com.androiddesdecero.arquitecturamvvm.ui.common.RetryCall;
 import com.androiddesdecero.arquitecturamvvm.utils.AutoClearedValue;
 
 import javax.inject.Inject;
@@ -56,4 +63,54 @@ public class SearchFragment extends Fragment implements Injectable {
         return dataBinding.getRoot();
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        searchViewModel = ViewModelProviders.of(this, viewModelProvider).get(SearchViewModel.class);
+        initRecyclerView();
+
+        RepoListAdapter rvAdapter = new RepoListAdapter(dataBindingComponent, true, new RepoListAdapter.RepoClickCallback() {
+            @Override
+            public void onClick(Repo repo) {
+                navigationController.navigateToRepo(repo.owner.login, repo.name);
+            }
+        });
+
+        binding.get().repoList.setAdapter(rvAdapter);
+        adapter = new AutoClearedValue<>(this, rvAdapter);
+
+        initSearchInputListener();
+
+        binding.get().setCallback(new RetryCall() {
+            @Override
+            public void retry() {
+                searchViewModel.refresh();
+            }
+        });
+    }
+
+    private void initSearchInputListener(){
+        binding.get().input.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH){
+                    SearchFragment.this.doSearch(v);
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        binding.get().input.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if((event.getAction() == KeyEvent.ACTION_DOWN)&& (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    doSearch(v);
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
 }
