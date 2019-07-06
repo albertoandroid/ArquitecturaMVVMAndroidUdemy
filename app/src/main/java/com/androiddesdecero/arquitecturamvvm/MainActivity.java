64090b2119d01dd77e8984d126ff138123e7caf6
Alberto <info@androiddesdecero.com>
2019-07-06 15:53:04 +0200

    83

--- app/src/main/java/com/androiddesdecero/arquitecturamvvm/MainActivity.java
+++ app/src/main/java/com/androiddesdecero/arquitecturamvvm/MainActivity.java
@@ -1,14 +1,36 @@
 package com.androiddesdecero.arquitecturamvvm;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.androiddesdecero.arquitecturamvvm.ui.common.NavigationController;
+
+import javax.inject.Inject;
+
+import dagger.android.DispatchingAndroidInjector;
+import dagger.android.support.HasSupportFragmentInjector;
+
+public class MainActivity extends AppCompatActivity implements HasSupportFragmentInjector {
+
+    @Inject
+    DispatchingAndroidInjector<Fragment> dispatchingAndroidInjector;
+
+    @Inject
+    NavigationController navigationController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(savedInstanceState == null){
+            navigationController.navigateToSearch();
+        }
+    }
+
+    @Override
+    public DispatchingAndroidInjector<Fragment> supportFragmentInjector(){
+        return dispatchingAndroidInjector;
     }
 }
