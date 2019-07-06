b47d3a2dacbaa51b8979a0b8e7a374a0fe224dcb
Alberto <info@androiddesdecero.com>
2019-07-06 15:48:58 +0200

    82

--- app/src/main/java/com/androiddesdecero/arquitecturamvvm/ui/common/NavigationController.java
+++ app/src/main/java/com/androiddesdecero/arquitecturamvvm/ui/common/NavigationController.java
@@ -5,6 +5,9 @@ import androidx.fragment.app.FragmentManager;
 
 import com.androiddesdecero.arquitecturamvvm.MainActivity;
 import com.androiddesdecero.arquitecturamvvm.R;
+import com.androiddesdecero.arquitecturamvvm.ui.repo.RepoFragment;
+import com.androiddesdecero.arquitecturamvvm.ui.search.SearchFragment;
+import com.androiddesdecero.arquitecturamvvm.ui.user.UserFragment;
 
 import javax.inject.Inject;
 
@@ -18,4 +21,30 @@ public class NavigationController {
         this.containerId = R.id.container;
         this.fragmentManager = mainActivity.getSupportFragmentManager();
     }
+
+    public void navigateToSearch(){
+        SearchFragment searchFragment = new SearchFragment();
+        fragmentManager.beginTransaction()
+                .replace(containerId, searchFragment)
+                .commitAllowingStateLoss();
+    }
+
+    public void navigateToRepo(String owner, String name){
+        RepoFragment fragment = RepoFragment.create(owner, name);
+        String tag = "repo" + "/" + "/" + name;
+        fragmentManager.beginTransaction()
+                .replace(containerId, fragment, tag)
+                .addToBackStack(null)
+                .commitAllowingStateLoss();
+    }
+
+    public void navigateToUser(String login){
+        String tag = "user" + "/" + login;
+        UserFragment fragment = UserFragment.create(login);
+        fragmentManager.beginTransaction()
+                .replace(containerId, fragment, tag)
+                .addToBackStack(null)
+                .commitAllowingStateLoss();
+    }
+
 }
