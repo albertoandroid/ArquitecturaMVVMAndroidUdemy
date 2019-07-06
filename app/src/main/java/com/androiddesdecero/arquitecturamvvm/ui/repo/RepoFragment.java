c1d218a960ffdf44077f5100696748ed7b690fe4
Alberto <info@androiddesdecero.com>
2019-07-06 10:10:30 +0200

    78

--- app/src/main/java/com/androiddesdecero/arquitecturamvvm/ui/repo/RepoFragment.java
+++ app/src/main/java/com/androiddesdecero/arquitecturamvvm/ui/repo/RepoFragment.java
@@ -3,25 +3,87 @@ package com.androiddesdecero.arquitecturamvvm.ui.repo;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingComponent;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.androiddesdecero.arquitecturamvvm.R;
+import com.androiddesdecero.arquitecturamvvm.binding.FragmentDataBindingComponent;
+import com.androiddesdecero.arquitecturamvvm.databinding.FragmentRepoBinding;
 import com.androiddesdecero.arquitecturamvvm.di.Injectable;
+import com.androiddesdecero.arquitecturamvvm.model.Contributor;
+import com.androiddesdecero.arquitecturamvvm.model.Repo;
+import com.androiddesdecero.arquitecturamvvm.repository.Resource;
+import com.androiddesdecero.arquitecturamvvm.ui.common.NavigationController;
+import com.androiddesdecero.arquitecturamvvm.utils.AutoClearedValue;
+
+import javax.inject.Inject;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class RepoFragment extends Fragment implements Injectable {
 
+    private static final String REPO_OWNER_KEY = "repo_owner";
+    private static final String REPO_NAME_KEY = "repo_name";
+
+    @Inject
+    ViewModelProvider.Factory viewModelFactory;
+
+    private RepoViewModel repoViewModel;
+
+    @Inject
+    NavigationController navigationController;
+
+    androidx.databinding.DataBindingComponent dataBindingComponent = new FragmentDataBindingComponent(this);
+
+    AutoClearedValue<FragmentRepoBinding> binding;
+    AutoClearedValue<ContributorAdapter> adapter;
+
 
     public RepoFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        repoViewModel = ViewModelProviders.of(this, viewModelFactory).get(RepoViewModel.class);
+        Bundle args = getArguments();
+        if(args != null && args.containsKey(REPO_OWNER_KEY) && args.containsKey(REPO_NAME_KEY)){
+            repoViewModel.setId(args.getString(REPO_OWNER_KEY), args.getString(REPO_NAME_KEY));
+        } else {
+            repoViewModel.setId(null, null);
+        }
+
+        LiveData<Resource<Repo>> repo = repoViewModel.getRepo();
+        repo.observe(this, new Observer<Resource<Repo>>() {
+            @Override
+            public void onChanged(Resource<Repo> resource) {
+                binding.get().setRepo(resource == null ? null : resource.data);
+                binding.get().setRepoResource(resource);
+                binding.get().executePendingBindings();
+            }
+        });
+
+        ContributorAdapter adapter = new ContributorAdapter(dataBindingComponent, new ContributorAdapter.ContributorClickCallback() {
+            @Override
+            public void onClick(Contributor contributor) {
+                navigationController.navigateToUser(contributor.getLogin());
+            }
+        });
+        this.adapter = new AutoClearedValue<>(this, adapter);
+        binding.get().contributorList.setAdapter(adapter);
+        initContributorList(repoViewModel);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
